a797a497000863cad1e8d81a760d0b1e87c32202
vaishnav <entbuddy12@gmail.com>
2022-02-08 08:16:29 +0530

    prettier etup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path'
+import { loadEnv } from 'vite'
+// Plugins
+import { ViteAliases } from 'vite-aliases'
+// import { injectHtml, minifyHtml } from 'vite-plugin-html';
+import svgr from 'vite-plugin-svgr'
+import react from 'vite-preset-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    build: {
+      // rollupOptions: {
+      //   input: glob.sync(path.resolve(__dirname, 'public', '*.html')),
+      // },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    plugins: [
+      react({ removeDevtoolsInProd: true, injectReact: true }),
+      svgr(),
+      ViteAliases(),
+      // minifyHtml(),
+      // injectHtml({
+      //   injectData: {
+      //     title: env.VITE_SITE_TITLE || 'App',
+      //   },
+      // }),
+    ],
+  }
+}
